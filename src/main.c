ce7a3594e452424401de3f519ef57498a8696b71
spy <alfred@stensatter.se>
2022-12-23 00:45:21 +0100

    made setting variable to own function

--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 /*
     Alfred Roos 2022-12-12
 */
-
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,15 +20,23 @@ int checkFlags(int argc, char *argv[]){
             if(strcmp(arg,"-d")==0){
                 debug = 1;
             }
+            else if(strcmp(arg,"-ip")==0){
+                struct node* expression = getExpression(argv[i+1]);
+                // translate infix linkedlist to posfix linkedlist
+                printStack(infixToPosfix(expression)," ");
+                return 0;
+            }
             else if(strcmp(arg,"-h")==0){
                 printf("calcer is a advenced calculator for writing\n");
                 printf("math expressions like '90-90/2-(2-3)'\n\n");
                 printf("-d enables debug mode(prints the stages to calculate)\n");
+                printf("-i followed by a math expression will calculate and output the value\n");
+                printf("-ip followed by a math expression will output posfix value\n");
                 printf("[expression] calculates the expression\n");
                 return 0;
             }
-            else{
-                strcpy(input,arg);
+            else if(strcmp(arg,"-i")==0){
+                strcpy(input,argv[i+1]);
             }
         }
     }
@@ -41,8 +48,56 @@ void calc(char* expressionStr){
     //parsed string to linkedlist
     struct node* expression = getExpression(expressionStr);
     // translate infix linkedlist to posfix linkedlist
-
     printf("%f",calculate(infixToPosfix(expression),varables));
+}
+
+void setVariable(char* string){
+    char varname[256];
+    memset(varname,0,256);
+    int equal = 0;
+    for(int i = 0;i<strlen(string);i++){
+        if(string[i]=='='){
+            break;
+        }
+        varname[i] = string[i];
+        equal++;            
+    }
+    // get value
+    char value[256];
+    memset(value,0,256);
+    for(int i = equal+1;i < strlen(string);i++){
+        value[i-equal-1] = string[i];
+    }
+
+    struct node* expression = getExpression(value);
+
+    // translate infix linkedlist to posfix linkedlist
+    float calculated = calculate(infixToPosfix(expression),varables);
+
+    int varLen = stackLen(varables);
+
+    struct map* variable = init_map();
+    //set varable struct to the one with the same name
+    for(int i = 0;i < varLen; i++ ){
+        if(debug==1){
+            printf("remap variable name");
+        }
+        struct node* vNode = (struct node*)get(varables,i);
+        if(strcmp(varname,((struct map*)vNode->value)->key)==0){
+            variable = ((struct map*)vNode->value);
+        }
+    }
+
+
+    variable->key = malloc(sizeof(char*));
+    memset(variable->key,0,sizeof(*variable->key));
+    strcpy(variable->key,varname);
+
+    variable->value = malloc(sizeof(float));
+    *variable->value = calculated;
+
+
+    push(varables,variable);
 }
 
 int checkPreDefined(char* string){
@@ -52,53 +107,7 @@ int checkPreDefined(char* string){
     }
     else if(strstr(string,"=")){
         // get variable name
-        char varname[256];
-        memset(varname,0,256);
-        int equal = 0;
-        for(int i = 0;i<strlen(string);i++){
-            if(string[i]=='='){
-                break;
-            }
-            varname[i] = string[i];
-            equal++;            
-        }
-        // get value
-        char value[256];
-        memset(value,0,256);
-        for(int i = equal+1;i < strlen(string);i++){
-            value[i-equal-1] = string[i];
-        }
-
-        struct node* expression = getExpression(value);
-
-        // translate infix linkedlist to posfix linkedlist
-        float calculated = calculate(infixToPosfix(expression),varables);
-
-        int varLen = stackLen(varables);
-
-        struct map* variable = init_map();
-        //set varable struct to the one with the same name
-        for(int i = 0;i < varLen; i++ ){
-            if(debug==1){
-                printf("remap variable name");
-            }
-            struct node* vNode = (struct node*)get(varables,i);
-            if(strcmp(varname,((struct map*)vNode->value)->key)==0){
-                variable = ((struct map*)vNode->value);
-            }
-        }
-
-
-        variable->key = malloc(sizeof(char*));
-        memset(variable->key,0,sizeof(*variable->key));
-        strcpy(variable->key,varname);
-
-        variable->value = malloc(sizeof(float));
-        *variable->value = calculated;
-
-
-        push(varables,variable);
-
+        setVariable(string);
         return 1;
     }
     else if(strcmp(string, "help")==0){
