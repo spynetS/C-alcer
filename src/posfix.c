2468b1da9e8e40a19686ed9fd799c3017c5b5967
spy <alfred@stensatter.se>
2022-12-28 23:16:11 +0100

    fixed perathesis

--- src/posfix.c
+++ src/posfix.c
@@ -1,188 +1,195 @@
-#include <stddef.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
 #include "stack.c"
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int debug = 0; // 1 will log what its doing
 
-int isOperator(char* operator){
-    if(*operator == '^') return 0;
-    if(*operator == '*') return 0;
-    if(*operator == '/') return 0;
-    if(*operator == '+') return 0;
-    if(*operator == '-') return 0;
-    return 1;
-}
-
-int getLevel(char* operator){
-    if(*operator == '^') return 2;
-    if(*operator == '*') return 1;
-    if(*operator == '/') return 1;
-    if(*operator == '+') return 0;
-    if(*operator == '-') return 0;
-
+int isOperator(char *operator) {
+  if (*operator== '^')
     return 0;
+  if (*operator== '*')
+    return 0;
+  if (*operator== '/')
+    return 0;
+  if (*operator== '+')
+    return 0;
+  if (*operator== '-')
+    return 0;
+  return 1;
 }
 
-void addOperator(struct node* stack,struct node* output,char* operator){
+int getLevel(char *operator) {
+  if (*operator== '^')
+    return 3;
+  if (*operator== '*')
+    return 2;
+  if (*operator== '/')
+    return 2;
+  if (*operator== '+')
+    return 1;
+  if (*operator== '-')
+    return 1;
 
-    if(stackLen(stack)>0){
-        int myLevel = getLevel(operator);
-
-        if(strcmp(peek(stack)->value,"(")==0){
-            pushstr(stack,operator);
-        }
-        //is operator
-        else{
-            while(stackLen(stack)>0 && 
-                myLevel <= getLevel(peek(stack)->value)){
-
-                if(debug == 1){
-                    printf("top %i\n",getLevel(peek(stack)->value));
-                    printf("new %i\n",myLevel);
-                }
-                pushstr(output,pop(stack)->value);
-            }
-            pushstr(stack,operator);
-        }
-    }else{
-        pushstr(stack,operator);
-    }
+  return -1;
 }
 
-//Returns a string in posfixform from a linkedlist of infix
-struct node* infixToPosfix(struct node *expression){
-    // holds the operators 
-    struct node* stack = init_stack();
-    // holds the operands
-    struct node* output = init_stack();
+void addOperator(struct node *stack, struct node *output, char *operator) {
 
-    int len = stackLen(expression);
+  if (stackLen(stack) > 0) {
+    int myLevel = getLevel(operator);
 
-    for(int i = 0; i < len;i++){
-        char* term = get(expression,i)->value;
-        if(strcmp(term,"(")==0){
-            pushstr(stack,term);
-        }
-        //pop all operators untill the next ( and pushstr top output
-        else if(strcmp(term,")")==0){
-            char* poped = pop(stack)->value; 
-            while(0==0){
-                pushstr(output,poped);
-
-                if(strcmp(peek(stack)->value,"(")==0) break;
-
-                poped = pop(stack)->value;
-            }
-            //remove the last (
-            pop(stack);
-        }
-        // if operator we do some magic (add in a special way)
-        else if(isOperator(term)==0){
-            addOperator(stack, output,term);
-        }else{
-            pushstr(output,term);
-        }
-
-        if(debug==1){
-            printf("r %s\nstack ",term);
-            printStack(stack," ");
-            printf("output ");
-            printStack(output," ");
-        }
+    // is operator
+    while (stackLen(stack) > 0 && myLevel <= getLevel(peek(stack)->value)) {
+      if (*(char *)peek(stack)->value == '(') {
+          pop(stack);
+          return;
+      }
+      if (debug == 1) {
+        printf("top %i\n", getLevel(peek(stack)->value));
+        printf("new %i\n", myLevel);
+      }
+      pushstr(output, pop(stack)->value);
     }
-    // add the remaining operat operators to the output 
-    int slen = stackLen(stack);
-    for(int i = 0; i < slen;i++){
-       pushstr(output,pop(stack)->value);
-    }
-    
-    /* printStack(stack," "); */
-    return output;
+    pushstr(stack, operator);
+
+  } else {
+    pushstr(stack, operator);
+  }
 }
 
-//checks for a operator followed by a negative sign and replaces it with(0-[next operand])
-char* parseNegativeNumbers(char* expressionStr,size_t size){
+// Returns a string in posfixform from a linkedlist of infix
+struct node *infixToPosfix(struct node *expression) {
+  // holds the operators
+  struct node *stack = init_stack();
+  // holds the operands
+  struct node *output = init_stack();
 
-    int isOp = 0;
-    char ex[256]; // new string
-    int count = 0;
-    int isNeg = 0;
-    for (char character = *expressionStr; character != '\0'; character = *++expressionStr){
-        if(isOperator(&character) == 0 ){
-            if(isOp == 1 && character == '-'){
-                ex[count] = '(';
-                count++;
-                ex[count] = '0';
-                isNeg = 1;
-                count++;
-            }
-            else if(isNeg == 1){
-                ex[count] = ')';
-                isNeg = 0;
-                count++;
-                continue;
-            }
-            isOp = 1; 
-        }
-        else{
-            isOp=0;
-        }
-        ex[count] = character;
+  int len = stackLen(expression);
 
+  for (int i = 0; i < len; i++) {
+    char *term = (char *)get(expression, i)->value;
+    if (strcmp(term,"(") == 0) {
+        pushstr(stack, term);
+    }
+    // pop all operators untill the next ( and pushstr top output
+    else if (strcmp(term,")") == 0 || *term == ')' ) {
+
+         char* poped = pop(stack)->value;
+         while(strcmp(poped,"(")!=0){
+            pushstr(output,poped);
+            if(strcmp(poped,"(")==0 || *term == '(') {
+                break;
+            }
+            poped = pop(stack)->value;
+        }
+    }
+    // if operator we do some magic (add in a special way)
+    else if (isOperator(term) == 0) {
+      addOperator(stack, output, term);
+    } else {
+      pushstr(output, term);
+    }
+
+    if (debug == 1) {
+      printf("r %s\nstack ", term);
+      printStack(stack, " ");
+      printf("output ");
+      printStack(output, " ");
+    }
+  }
+  // add the remaining operat operators to the output
+  int slen = stackLen(stack);
+  for (int i = 0; i < slen; i++) {
+    pushstr(output, pop(stack)->value);
+  }
+
+  /* printStack(stack," "); */
+  return output;
+}
+
+// checks for a operator followed by a negative sign and replaces it
+// with(0-[next operand])
+char *parseNegativeNumbers(char *expressionStr, size_t size) {
+
+  int isOp = 0;
+  char ex[256]; // new string
+  int count = 0;
+  int isNeg = 0;
+  for (char character = *expressionStr; character != '\0';
+       character = *++expressionStr) {
+    if (isOperator(&character) == 0) {
+      if (isOp == 1 && character == '-') {
+        ex[count] = '(';
         count++;
-    }
-    if(isNeg == 1){
+        ex[count] = '0';
+        isNeg = 1;
+        count++;
+      } else if (isNeg == 1) {
         ex[count] = ')';
+        isNeg = 0;
         count++;
+        continue;
+      }
+      isOp = 1;
+    } else {
+      isOp = 0;
     }
-    if(debug==1){
-        printf("t %p\n",expressionStr);
-    }
-    ex[count] = '\0';
-    return strdup(ex);     
+    ex[count] = character;
+
+    count++;
+  }
+  if (isNeg == 1) {
+    ex[count] = ')';
+    count++;
+  }
+  if (debug == 1) {
+    printf("t %p\n", expressionStr);
+  }
+  ex[count] = '\0';
+  return strdup(ex);
 }
 
- /*
+/*
  *Infix string addded all operands and operators to a linkded list
  **/
-struct node* getExpression(char* expressionS){
-    
-    char* expressionStr = parseNegativeNumbers(expressionS, sizeof(expressionS)); 
+struct node *getExpression(char *expressionS) {
 
-    if(debug==1){
-        printf("after negative check %s\n",expressionStr);
+  char *expressionStr = parseNegativeNumbers(expressionS, sizeof(expressionS));
+
+  if (debug == 1) {
+    printf("after negative check %s\n", expressionStr);
+  }
+
+  struct node *expression = init_stack();
+
+  char word[256];
+  memset(word, 0, 256);
+
+  int count = 0;
+  for (char character = *expressionStr; character != '\0';
+       character = *++expressionStr) {
+    // if the char is a operator add the prev word to the list
+    if (isOperator(&character) == 0 || character == '(' || character == ')') {
+      if (strlen(word) > 0) {
+        pushstr(expression, word);
+        /* printStack(expression,":"); */
+      }
+      pushstr(expression, &character);
+      memset(word, 0, sizeof(word));
+      count = 0;
+      continue;
+    } else {
+      word[count] = character;
     }
 
-    struct node* expression = init_stack();
+    count++;
+  }
+  pushstr(expression, word);
+  if (debug == 1) {
+    printStack(expression, " ");
+  }
 
-    char word[256];
-    memset(word,0,256);
-
-    int count = 0;
-    for (char character = *expressionStr; character != '\0'; character = *++expressionStr){
-        // if the char is a operator add the prev word to the list
-        if(isOperator(&character)==0 || character=='(' || character == ')' ){
-            if(strlen(word)>0){
-                pushstr(expression,word);
-                /* printStack(expression,":"); */
-            }
-            pushstr(expression,&character);
-            memset(word,0,sizeof(word));
-            count=0;
-            continue;
-        }
-        else{
-            word[count] = character;
-        }
-
-        count++;
-    }
-    pushstr(expression,word);
-    if(debug==1){
-        printStack(expression," ");
-    }
-
-    return expression;
+  return expression;
 }
